c242b1364781bc817f0ca3597c305eff841f6270
Nu424 <murataka0424@gmail.com>
2026-04-13 02:31:11 +0900

    change: mainで、各種アダプタの実行例を記述した - ついでに、デフォルトでGUIアダプタを表示してみることにした

--- main.py
+++ main.py
@@ -4,15 +4,19 @@
 その service に HTTP 経由でアクセスする。
 """
 
-from stationkit import MockStationController, create_cli_app
+from stationkit import MockStationController
+# from stationkit import create_cli_app
+# from stationkit import create_http_app
+from stationkit import create_gui_app
 
-app = create_cli_app(MockStationController())
+controller = MockStationController()
 
-
-def main() -> None:
-    """Typer アプリを起動する（``python -m`` やコンソールスクリプトから呼ばれる）。"""
-    app()
+# cli_app = create_cli_app(controller)
+# http_app = create_http_app(controller)
+gui_app = create_gui_app(controller)
 
 
 if __name__ == "__main__":
-    main()
+    # cli_app()
+    # http_app.run()
+    gui_app.launch()
